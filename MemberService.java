8d73594f5eca9aa340b70ab70771565d6f061c06
Jeong-Dong-gi <159031134+Jeong-Dong-gi@users.noreply.github.com>
2024-06-17 00:29:26 +0900

    interface

---
+++ MemberService.java
@@ -0,0 +1,12 @@
+package com.testboard2.service;
+
+import org.springframework.stereotype.Service;
+
+import com.testboard2.dto.MemberDTO;
+
+@Service
+public interface MemberService {
+
+	public void insertMember(MemberDTO memberDTO);
+	
+}
